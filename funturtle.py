93470c2b10163972f6a54d5826ee5bdc5c9efd8c
omer19-meet <omer19@meet.mit.edu>
2017-07-31 18:32:34 +0300

    Stopping here

--- funturtle.py
+++ funturtle.py
@@ -19,12 +19,14 @@ import turtle
 ##
 ##triangle.goto(50,100)
 ##triangle.goto(0,0)
+turtle.pendown()
 
 UP_ARROW = "Up"
 LEFT_ARROW = "Left"
 DOWN_ARROW = "Down"
 RIGHT_ARROW = "Right"
 SPACEBAR = "space"
+
 
 UP = 0
 LEFT = 1
@@ -36,17 +38,33 @@ direction = UP
 def up():
     global direction
     direction = UP
+    print(direction)
     return("you pressed Up")
 
 def left():
     global direction
-    global UP
-    global LEFT
-    global 
     direction = LEFT
     print(direction)
     return("you pressed Left")
 
+def down():
+    global direction
+    direction = DOWN
+    print(direction)
+    return("you pressed Down")
+           
+def right():
+    global direction
+    direction = RIGHT
+    print(direction)
+    return("you pressed Right")
 
+turtle.onkeypress(up, UP_ARROW)
+turtle.onkeypress(down, DOWN_ARROW)
+turtle.onkeypress(left, LEFT_ARROW)
+turtle.onkeypress(right, RIGHT_ARROW)
+
+turtle.listen()
+print("Alexander the GREAT")
 turtle.mainloop()
 
